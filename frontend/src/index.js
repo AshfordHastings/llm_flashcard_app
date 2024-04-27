2d6a35f79099553c1c5783c5e133ec60e0100673
Ashford Hastings <ashfordhastings@MacBook-Pro.attlocal.net>
2024-04-27 17:43:29 -0500

    update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';;
 
-axios.defaults.baseURL = process.env.BACKEND_URL || 'http://localhost:5001/';
+axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
