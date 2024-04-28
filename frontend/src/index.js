6952dbb7182f575e840b8e4e5ccf447895ab6a01
Ashford Hastings <33183721+AshfordHastings@users.noreply.github.com>
2024-04-28 14:29:45 -0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';;
+import App from './App';
 
 axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5001/';
 
